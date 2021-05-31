8a9e73e5d903defa0597ae15bda0f573925417cd
Jose Mancilla <josemancillamarambio@gmail.com>
2021-05-31 17:11:58 -0400

    Menu estatico

--- src/Platzi/Doctor.java
+++ src/Platzi/Doctor.java
@@ -1,11 +1,24 @@
 package Platzi;
 
-public class Doctor {
-    private int id;
-    String name;
-    String Speciality;
+import javax.print.Doc;
 
+public class Doctor {
+    //Atributos
+    int id;
+    String name;
+    String speciality;
+
+    Doctor(){
+        System.out.println("Construyendo el Objeto Doctor");
+    }
+
+    Doctor(String name){
+        System.out.println("El nombre del Doctor asignado es: " + name);
+    }
+
+    //Comportamientos
     public void showName(){
         System.out.println(name);
     }
+
 }
--- src/Platzi/Main.java
+++ src/Platzi/Main.java
@@ -1,10 +1,13 @@
 package Platzi;
+import static Platzi.Ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "José Mancilla Marambio";
-        myDoctor.showName();
+        showMenu();
+
 
     }
+
+
+
 }
